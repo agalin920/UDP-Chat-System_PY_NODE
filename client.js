6aa353c90a1b85b7b73c1048faf6f8523c517f7d
Jorge Ruvalcaba <jorge.ruvalcaba@scalablepress.com>
2018-04-27 22:28:25 -0500

    Added exit func and user display

--- client.js
+++ client.js
@@ -1,72 +1,82 @@
-process.stdin.resume();//so the program will not close instantly
-var createSocket = require('dgram').createSocket;
-var readline = require('readline');
+process.stdin.resume(); //so the program will not close instantly
+let createSocket = require('dgram').createSocket;
+let readline = require('readline');
+let util = require('util')
 const client = createSocket('udp4');
 
-var SERVER_PORT = 3000;
-var SERVER_HOST = '127.0.0.1';
+let SERVER_PORT = 3000;
+let SERVER_HOST = '127.0.0.1';
 
-var user = null;
+let user = null;
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 client.on('error', (err) => {
-  console.log(`client error:\n${err.stack}`);
+  console.log(`Client error:\n${err.stack}`);
 });
 
 client.on('message', (msg, rinfo) => {
   let js = msg.toString('utf8');
   let json = JSON.parse(js);
 
-  if(json.body.sender){
-    if(json.body.sender != user){
-      process.stdout.clearLine();  
-      process.stdout.cursorTo(0); 
-      console.log(json.body.sender+":"+json.body.message);
-      process.stdout.write('you: ');
-    }
+  if(json.body.sender && json.body.sender != user){
+    process.stdout.clearLine();  
+    process.stdout.cursorTo(0); 
+    console.log(`${json.body.sender}: ${json.body.message}`);
+    process.stdout.write(`${user}: `);
   }
-  
-  
-  
 });
 
 client.on('listening', () => {
-  var address = client.address();
-  process.stdout.write('you: ');
+  let address = client.address();
+  process.stdout.write(`${user}: `);
 });
 
-
-rl.question('Please enter a username ', (answer) => {
+rl.question('Welcome to the coolest chat\nPlease enter a username: ', (answer) => {
   user = answer;
   let msg = {
-        'header': {
-          'type': 'connecting'
-        },
-        'body': {
-          'user': `${user}`,
-        }
-      }
+    'header': {
+      'type': 'connecting'
+    },
+    'body': {
+      'user': `${user}`,
+    }
+  }
   let message = new Buffer(JSON.stringify(msg));
   client.send(message, 0, message.length, SERVER_PORT, SERVER_HOST);
   //process.stdout.write('you: ');
 });
 
-
 rl.on('line', (input) => {
-  process.stdout.write('you: ');
+  process.stdout.write(`${user}: `);
   let msg = {
-        'header': {
-          'type': 'Sending'
-        },
-        'body': {
-          'sender': `${user}`,
-          'message': `${input}`
-        }
-      }
+    'header': {
+      'type': 'Sending'
+    },
+    'body': {
+      'sender': `${user}`,
+      'message': `${input}`
+    }
+  }
+  execCommand(input.trim());
   let message = JSON.stringify(msg);
   client.send(message, 0, message.length, SERVER_PORT, SERVER_HOST);
+}).on('close', () => {
+  //it gets triggered by ^C or ^D
+  util.puts('\nGoodbye!');
+  process.exit(0);
 });
 
+function execCommand(command) {
+  switch (command) {
+    case '4':
+    case 'exit':
+    case 'quit':
+    case 'salir':
+    case 'q':
+      process.exit(0);
+      break;
+  }
+}
