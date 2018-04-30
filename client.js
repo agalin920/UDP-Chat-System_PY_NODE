aef9ac9bdade7f77154cec479854d638ee084c19
Andres Galindo <agalin920@gmail.com>
2018-04-30 09:37:58 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,64 +1,89 @@
-process.stdin.resume();//so the program will not close instantly
-var createSocket = require('dgram').createSocket;
-var readline = require('readline');
+process.stdin.resume(); //so the program will not close instantly
+let createSocket = require('dgram').createSocket;
+let readline = require('readline');
+let util = require('util')
 const client = createSocket('udp4');
 
-var SERVER_PORT = 3000;
-var SERVER_HOST = '127.0.0.1';
+let SERVER_PORT = 3000;
+let SERVER_HOST = undefined;
 
-var user = null;
+let user = null;
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 client.on('error', (err) => {
-  console.log(`client error:\n${err.stack}`);
+  console.log(`Client error:\n${err.stack}`);
 });
 
 client.on('message', (msg, rinfo) => {
-  let messageObject = JSON.parse(msg.toString());
-  if(messageObject.header.type === 'close') {
-    console.log(Buffer.from(messageObject.body.message).toString());
-    process.exit();
-  } else if(messageObject.header.type === 'Sending') {
-    console.log(Buffer.from(messageObject.body.message).toString());
-  } else {
-    console.log('unknown message');
+  let js = msg.toString('utf8');
+  let json = JSON.parse(js);
+
+  if(json.body.sender && json.body.sender != user){
+    process.stdout.clearLine();  
+    process.stdout.cursorTo(0); 
+    console.log(`${json.body.sender}: ${json.body.message}`);
+    process.stdout.write(`${user}: `);
   }
 });
 
 client.on('listening', () => {
-  var address = client.address();
+  let address = client.address();
+  process.stdout.write(`${user}: `);
 });
 
-
-rl.question('Please enter a username ', (answer) => {
-  user = answer;
-  let msg = {
-        'header': {
-          'type': 'connecting'
-        },
-        'body': {
-          'user': `${user}`,
-        }
+rl.question('What is the ip of the server: ', (answer) => {
+  SERVER_HOST = answer;
+  rl.question('Welcome to the coolest chat\nPlease enter a username: ', (answer) => {
+    user = answer;
+    let msg = {
+      'header': {
+        'type': 'connecting'
+      },
+      'body': {
+        'user': `${user}`,
       }
-  let message = new Buffer(JSON.stringify(msg));
-  client.send(message, 0, message.length, SERVER_PORT, SERVER_HOST);
+    }
+    let message = new Buffer(JSON.stringify(msg));
+    client.send(message, 0, message.length, SERVER_PORT, SERVER_HOST);
+    //process.stdout.write('you: ');
+    
+  });
 });
 
 rl.on('line', (input) => {
+  process.stdout.write(`${user}: `);
   let msg = {
-        'header': {
-          'type': 'Sending'
-        },
-        'body': {
-          'sender': `${user}`,
-          'message': `${input}`
-        }
-      }
+    'header': {
+      'type': 'Sending'
+    },
+    'body': {
+      'sender': `${user}`,
+      'message': `${input}`
+    }
+  }
+  execCommand(input.trim());
   let message = JSON.stringify(msg);
-  console.log(message);
   client.send(message, 0, message.length, SERVER_PORT, SERVER_HOST);
-  console.log(`you: ${input}`);
+}).on('close', () => {
+  //it gets triggered by ^C or ^D
+  util.puts('\nGoodbye!');
+  process.exit(0);
 });
+
+function execCommand(command) {
+  switch (command) {
+    case '4':
+    case 'exit':
+    case 'quit':
+    case 'salir':
+    case 'q':
+      process.exit(0);
+      break;
+    case 'users':
+      let users = 'users';
+      client.send(users, 0, users.length, SERVER_PORT, SERVER_HOST);
+  }
+}
