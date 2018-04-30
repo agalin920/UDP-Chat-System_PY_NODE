2ce7954aa1da4418c2bbd39f497c4a8cae618e0d
Andres Galindo <agalin920@gmail.com>
2018-04-30 09:35:12 -0500

    Update chatserver.py

--- chatserver.py
+++ chatserver.py
@@ -9,8 +9,8 @@ def run():
 	serv.bind(('',3000))
 
 	people={}
-	ports=[]
 	while 1:
+		#data,addr=serv.recvfrom(65536)
 		data,addr=serv.recvfrom(65536)
 		if addr[0] not in people.keys():
 			if data not in people.values():
@@ -18,18 +18,13 @@ def run():
 			else:
 				people[addr[0]] = data + "_"
 			continue
-
-		if addr[1] not in ports:
-			ports.append(addr[1])
-
 		data = data.strip()
 		if data != '':
 			print addr,": ",data
 			for i in people:
-				for j in ports:
 				#(data ,(destinatinoAddress, port)
 				#Random port because client not handling incoming data properly
-					serv.sendto(data,(i,j))
+				serv.sendto(people[addr[0]]+": "+data,(i,6668))
 	serv.close()
 if __name__ == "__main__":
 	run()
